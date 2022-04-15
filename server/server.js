47a9212d2ea80fb9d8ad9d32eeb220b01aa91032
svnday <40030806+svnday@users.noreply.github.com>
2022-04-14 22:02:34 -0500

    uncommenting routes so users can save books

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
+const routes = require('./routes');
 
 //import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -12,7 +12,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
